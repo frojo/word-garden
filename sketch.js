2ad30bbb51642b88119585f807855c9ab16b82d0
frojo <mojoz0101@gmail.com>
2020-03-11 15:53:58 -0400

    first commit

---
+++ sketch.js
@@ -0,0 +1,33 @@
+
+// list of rigidbodies in the scene
+// var bodies;
+
+function setup() {
+//   let body = new RigidBody(50, 50, 100, 100);
+//   bodies = {body};
+  createCanvas(640, 640);
+}
+
+function draw() {
+//   for (let i = 0; i < bodies.length; i++) {
+//     bodies.draw()
+//   }
+
+  background(220, 0, 220);
+  // ellipse(50, 50, 80, 80);
+}
+
+
+// 
+// RigidBody = function(x, y, w, h) {
+//   this.x = x;
+//   this.y = y;
+// 
+// }
+// 
+// RigidBody.draw = function() {
+//   rect(this.x, this.y, this.w, this.h)
+// 
+// }
+
+
