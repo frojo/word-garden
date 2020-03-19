386fee633395d676b46c69cd1bf0c2f8f836b254
frojo <mojoz0101@gmail.com>
2020-03-19 14:52:36 -0400

    change to non minified

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var server = app.listen(3000)
 
 app.use(express.static('public'));
 
-app.get('/p5-bbox-aligned-text.min.js', function(req, res) {
+app.get('/p5-bbox-aligned-text.js', function(req, res) {
       console.log('run our custom get func');
-      res.sendFile(__dirname + '/node_modules/p5-bbox-aligned-text/dist/bbox-aligned-text.min.js');
+      res.sendFile(__dirname + '/node_modules/p5-bbox-aligned-text/dist/bbox-aligned-text.js');
 });
